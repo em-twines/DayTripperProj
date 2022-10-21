292b1bb99566817069a8373830937a720ae7a4d4
emilytwines <emilytwines@users.noreply.github.com>
2022-10-21 12:15:42 -0400

    Project Description added, complete.

---
+++ day_tripper_proj.py
@@ -0,0 +1,12 @@
+#Project Description: 
+
+
+# 3 commits
+# store destinations, restaurants, modes of transport, and enterntainment as lists
+# randomly select each of the above (4 things)
+# randomly reselect any of the above uniquely
+# confirm that it is complete once all te selections are approved
+# display the completed trip in the console
+# abide by the Single Responsibility principle
+
+
